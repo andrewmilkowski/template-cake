d120b3a1653c6f4086db138a259d6baa4a1f60fe
Andrew Milkowski <andrewmilkowski@gmail.com>
2010-09-18 15:43:11 -0400

    first commit

---
+++ generate.sh
@@ -0,0 +1,13 @@
+# http://book.cakephp.org/view/915/Advanced-Installation
+
+#! --- one time only --
+#git clone http://github.com/cakephp/cakephp.git libs/vendor/cake/git
+
+#cp -r libs/vendor/cake/git/app/* .
+
+#cp config/database.php.default config/database.php
+
+#chmod 777 tmp
+#chmod 777 tmp/cache
+#chmod 777 tmp/cache/persistent
+#chmod 777 tmp/cache/models
